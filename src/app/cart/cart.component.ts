2f5e57796e681b0c306325175991eaf6b725bf04
KarthikBR <karthikbr43@gmail.com>
2019-11-13 12:36:18 +0530

    Checkout and Purchase with User name and address

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -8,12 +9,25 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent implements OnInit {
   items;
+  checkoutForm;
   constructor(
-    private cart: CartService
-  ) { }
-
-  ngOnInit() {
-this.items = this.cart.GetItems();
+    private cart: CartService,
+    private formBuider: FormBuilder,    /*Injecting FormBuilder*/
+  ) { 
+    this.items = this.cart.GetItems();
+    this.checkoutForm = this.formBuider.group({
+      name: '',
+      address: ''
+    })
   }
+  ngOnInit() {
+  }
+  onSubmit(customerdata){
+    // Process checkout data here 
+    console.warn('Your Order has been submitted',customerdata);
 
+    alert('Your Order has been placed successfully..');
+    this.items=this.cart.ClearCart();
+    this.checkoutForm.reset();
+  }
 }
